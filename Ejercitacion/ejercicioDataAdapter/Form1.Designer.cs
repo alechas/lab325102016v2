d178a29709358308435885f302a6e66ada0c9864
alexischas@hotmail.com <alexischas@hotmail.com>
2016-10-25 21:42:25 -0300

    dejo compilado el programa

---
+++ Ejercitacion/ejercicioDataAdapter/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ejercicioDataAdapter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.acciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarFillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acciónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(617, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(597, 223);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // acciónToolStripMenuItem
+            // 
+            this.acciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarFillToolStripMenuItem});
+            this.acciónToolStripMenuItem.Name = "acciónToolStripMenuItem";
+            this.acciónToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.acciónToolStripMenuItem.Text = "Acción";
+            // 
+            // cargarFillToolStripMenuItem
+            // 
+            this.cargarFillToolStripMenuItem.Name = "cargarFillToolStripMenuItem";
+            this.cargarFillToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cargarFillToolStripMenuItem.Text = "Cargar(Fill)";
+            this.cargarFillToolStripMenuItem.Click += new System.EventHandler(this.cargarFillToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem acciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarFillToolStripMenuItem;
+    }
+}
+
---
+++ Ejercitacion/ejercicioDataAdapter/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ejercicioDataAdapter
+{
+    public partial class Form1 : Form
+    {
+        private DataSet _dataSet;
+        private SqlDataAdapter _dataAdapter;
+        private SqlCommand _Select;
+        private SqlCommand _Insert;
+        private SqlCommand _Update;
+        private SqlCommand _Delete;
+        private SqlConnection _Connection;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Instanciar la conexión
+            this._Connection = new SqlConnection(Properties.Settings.Default.connDBAdapter);
+
+            this._dataSet = new DataSet();
+
+            //SqlCommand comando = new SqlCommand();
+            //comando.CommandText = "Select * from Clientes";
+            //comando.Connection = this._Connection;
+            //this._dataAdaptertaAdapter = new SqlDataAdapter(comando);//asi le estoy pasando el selectCommand
+
+            //Que la tabla ocupe todo el data grid
+            this.dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+
+            this._dataAdapter = new SqlDataAdapter("Select * from Clientes", this._Connection);//asi le estoy pasando el selectCommand
+
+        }
+
+        private void cargarFillToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this._dataAdapter.Fill(this._dataSet, "Clientes");
+
+                this.dataGridView1.DataSource = this._dataSet.Tables["Clientes"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
